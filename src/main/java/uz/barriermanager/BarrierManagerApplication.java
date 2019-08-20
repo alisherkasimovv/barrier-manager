887e105a3de659b6ac4a9de477158006268596a4
Alisher Kasimov <alisherkasimov1994@gmail.com>
2019-08-20 11:27:25 +0500

    Initial commit.

---
+++ src/main/java/uz/barriermanager/BarrierManagerApplication.java
@@ -0,0 +1,13 @@
+package uz.barriermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarrierManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarrierManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uz/barrier/barriermanager/BarrierManagerApplicationTests.java
@@ -0,0 +1,16 @@
+package uz.barrier.barriermanager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BarrierManagerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
